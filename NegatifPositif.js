087f91abe6a78736bc3e401a79f06230192386d8
nomenjanaharyginot6-ux <nomenjanaharyginot6@gmail.com>
2026-09-18 13:27:03 +0300

    add NegatifPositif.js

--- NegatifPositif.js
+++ NegatifPositif.js
@@ -1,3 +1,26 @@
-function NegatifPositif([]){
-    
+function NegatifPositif(T) {
+    let gauche = 0;
+    let droite = T.length - 1;
+
+    while (gauche < droite) {
+        while (gauche < droite && T[gauche] <= 0) {
+            gauche++;
+        }
+
+        while (gauche < droite && T[droite] > 0) {
+            droite--;
+        }
+
+        if (gauche < droite) {
+            let temporaire = T[gauche];
+            T[gauche] = T[droite];
+            T[droite] = temporaire;
+        }
+    }
+
+    return T;
 }
+
+let T = [3, -1, 0, 4, -2, 5, -3];
+
+console.log(NegatifPositif(T));
