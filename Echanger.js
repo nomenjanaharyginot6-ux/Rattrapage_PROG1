5d90d83f76cd2d13fff5a6e4a6f5481fe50a58de
nomenjanaharyginot6-ux <nomenjanaharyginot6@gmail.com>
2026-09-18 13:49:50 +0300

    add Echanger.js

---
+++ Echanger.js
@@ -0,0 +1,11 @@
+function Echanger(T, index1, index2) {
+    let temporaire = T[index1];
+    T[index1] = T[index2];
+    T[index2] = temporaire;
+}
+
+let T = [0, 1, 0];
+
+Echanger(T, 1, 2);
+
+console.log(T);
